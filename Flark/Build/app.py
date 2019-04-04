602763b16ad0f43bb30ca742e10303f4269f38ce
josiahbjorgaard <josiahbjorgaard@gmail.com>
2019-04-03 18:01:35 -0700

    added example and optional input to front end

--- Flark/Build/app.py
+++ Flark/Build/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route("/",methods = ['POST', 'GET'])
 def form():
     if request.method == 'POST': #this block is only entered when the form is submitted
-        example = request.form.get('example')
+        sparkinput = request.form.get('sparkinput')
         nodes = request.form.get('nodes')
-        command = ['sh','run-spark-test.sh',nodes]
+        example = request.form.get('example')
+        command = ['sh','run-spark-test.sh',nodes,example,sparkinput]
         process = subprocess.Popen(command, stdout = subprocess.PIPE)
         print("Running")
         output, err = process.communicate()
@@ -16,7 +17,8 @@ def form():
 
     return '''<form method="POST">
                   Number of Nodes: <input type="text" name="nodes"><br>
-                  Spark Example: <input type="text" name="example"><br>
+                  Spark Example: <input type="text" name='example"><br>
+                  Spark Input: <input type="text" name="sparkinput"><br>
                   <input type="submit" value="Submit"><br>
               </form>'''
 
